2fa40c0652ac664f2c5cd966aa0f1d3f53050b62
Based InvisibleTree <invisibletree@protonmail.ch>
2021-03-02 12:27:57 +0000

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ["./_site/**/*.html"],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  corePlugins: {
+    preflight: false,
+  },
+  plugins: [],
+};
