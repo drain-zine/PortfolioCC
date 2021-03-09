6ab8a1714361fc990bbc7b5344b9895f2a251570
Based InvisibleTree <invisibletree@protonmail.ch>
2021-03-09 13:56:44 +0000

    Added timeout bits

--- scripts/headerRandomizer.js
+++ scripts/headerRandomizer.js
@@ -1,57 +1,58 @@
 export function setRandomPos(elements, dx, dy, x, y) {
   var boxDims = new Array();
+  var timeout_iters = 10;
+  var timeout = false;
 
-  elements.each(function () {
-    var conflict = true;
-    var fixLeft = 0;
-    var fixTop = 0;
+  do {
+    elements.each(function () {
+      var fixLeft = 0;
+      var fixTop = 0;
+      var conflict = true;
+      var t = 0;
+      timeout = false;
 
-    while (conflict) {
-      fixLeft = Math.abs(Math.round(Math.random() * dx) + x);
-      fixTop = Math.abs(Math.round(Math.random() * dy) + y);
+      while (conflict) {
+        fixLeft = Math.abs(Math.round(Math.random() * dx) + x);
+        fixTop = Math.abs(Math.round(Math.random() * dy) + y);
 
-      $(this).css("top", fixTop);
-      $(this).css("left", fixLeft);
+        $(this).css("top", fixTop);
+        $(this).css("left", fixLeft);
 
-      var rect1 = $(this)[0].getBoundingClientRect();
+        var rect1 = $(this)[0].getBoundingClientRect();
 
-      conflict = false;
-      for (var i = 0; i < boxDims.length; i++) {
-        var rect2 = boxDims[i].getBoundingClientRect();
-        if (overlap(rect1, rect2)) {
-          conflict = true;
+        conflict = false;
+        for (var i = 0; i < boxDims.length; i++) {
+          var rect2 = boxDims[i].getBoundingClientRect();
+          if (overlap(rect1, rect2)) {
+            conflict = true;
+            t++;
+            break;
+          } else {
+            conflict = false;
+          }
+        }
+
+        // catch timeout
+        if (t > timeout_iters) {
+          timeout = true;
+          //console.log("Timing out...")
           break;
-        } else {
-          conflict = false;
         }
       }
-    }
-    boxDims.push($(this)[0]);
-  });
+
+      // reset elems on timeout, try again
+      if (timeout) {
+        boxDims = [];
+        return false;
+      } else {
+        boxDims.push($(this)[0]);
+      }
+    });
+  } while (timeout);
 }
 
 function overlap(rect1, rect2) {
   //console.log("checking overlap");
-
-  console.log(
-    "Dims (t,b,r,l): (" +
-      rect1.top +
-      "," +
-      rect1.bottom +
-      "," +
-      rect1.right +
-      "," +
-      rect1.left +
-      "), (" +
-      rect2.top +
-      "," +
-      rect2.bottom +
-      "," +
-      rect2.right +
-      "," +
-      rect2.left +
-      ")"
-  );
 
   return !(
     rect1.top > rect2.bottom ||
