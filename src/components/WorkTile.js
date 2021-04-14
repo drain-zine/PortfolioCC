b95be0201bbaa04afbd40653a2446067dcebb8b2
Tom Stannett <invisibletree@protonmail.ch>
2021-04-14 13:44:39 +0100

    init design

--- src/components/WorkTile.js
+++ src/components/WorkTile.js
@@ -20,20 +20,20 @@ const WorkTile = ({data}) => {
 
             <div style={{width: width+"rem" , height: height+"rem", left: init[0]+"%", top: init[1]+"%"}} className="draggable tile absolute rounded-md shadow-md overflow-y-hidden" onDoubleClick={toggleWorkTile}>
                 <div className={"preview w-full h-full absolute z-0"}>
-                    <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 text-4xl">{data.title}</p>
+                    <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 text-4xl select-none">{data.title}</p>
                     <img className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full h-full rounded-md shadow-md z-0" src={data.img} />
                 </div>
 
                 <BackgroundScrollTransistion className={"link w-full h-full absolute flex flex-col justify-center items-center text-white z-10"} trigger={expanded} >
                     
-                    <div className="text-left flex-inital">
-                        <p>2021</p>
+                    <div className="w-full flex text-left flex-inital">
+                     
                     </div>
                     
                     <div className="flex-1 flex flex-row justify-center items-center">
                         <div className="flex-1">
                             <div className="transform -rotate-90">
-                                <h1 className="text-6xl">{data.title.toUpperCase()}</h1>
+                                <h1 className="text-6xl text-red-900">{data.title.toUpperCase()}</h1>
                                 <p className="text-sm text-center mt-4">LOREM IPSUM LA LASFG ASWAG MONEY WEED XANS</p>
                             </div>
                         </div>
@@ -43,8 +43,13 @@ const WorkTile = ({data}) => {
                         </div>
                     </div>
 
-                    <div className="text-right flex-inital">
-                        <p>2021</p>
+                    <div className="w-full text-right flex-inital flex">
+                        <div className="flex-1 relative" style={{height: "24px"}}>
+                            <svg width="100%" height="100%" viewBox="0 0 100% 100%">
+                                <line class="point" x1="0" x2="100%" y1="50%" y2="50%" />
+                            </svg>
+                        </div>
+                        <p className="flex-inital mx-2 text-red-900">LINK</p>
                     </div>
                     
 
