e8703d78b5c3a1873608ce80e7bf49517f8ded96
Tom Stannett <invisibletree@protonmail.ch>
2021-04-14 11:08:30 +0100

    Toggling works

--- src/components/WorkTile.js
+++ src/components/WorkTile.js
@@ -22,9 +22,13 @@ const WorkTile = ({data}) => {
     return(
 
             <div style={{width: width+"rem" , height: height+"rem", left: init[0]+"%", top: init[1]+"%"}} className="draggable tile absolute rounded-md shadow-md" onDoubleClick={toggleWorkTile}>
-                <div>
-                <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 text-4xl">{data.title}</p>
-                <img className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full h-full rounded-md shadow-md z-0" src={data.img} />
+                <div className={"preview w-full h-full absolute z-10 "+ (!expanded ? "z-10" : "z-0")}>
+                    <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 text-4xl">{data.title}</p>
+                    <img className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full h-full rounded-md shadow-md z-0" src={data.img} />
+                </div>
+
+                <div className={"link w-full h-full absolute text-white " + (expanded ? "z-10" : "z-0")}>
+                    <p>LINK</p>
                 </div>
             </div>
     );
