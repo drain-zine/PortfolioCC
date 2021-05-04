805c6712bee85e07328aa10a99d90245e5ef441b
Tom Stannett <invisibletree@protonmail.ch>
2021-05-04 20:07:40 +0100

    Updates + demo

--- src/components/WorkTile.js
+++ src/components/WorkTile.js
@@ -24,7 +24,7 @@ const WorkTile = ({data, incrementTogglesCounter, decrementTogglesCounter}) => {
 
     return(
 
-            <FadeInDiv style={{width: width+"rem" , height: height+"rem", left: init[0]+"%", top: init[1]+"%"}} className="draggable tile absolute overflow-y-hidden" onDoubleClick={toggleWorkTile}>
+            <FadeInDiv style={{width: width+"rem" , height: height+"rem", left: init[0]+"%", top: init[1]+"%"}} className="draggable tile absolute overflow-hidden" onDoubleClick={toggleWorkTile}>
                 <div className={"preview w-full h-full absolute z-0"}>
                     <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 text-4xl select-none">{data.title}</p>
                     <img className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full h-full z-0" src={require(("./../data/assets/"+data.img)).default} />
@@ -47,7 +47,7 @@ const WorkTile = ({data, incrementTogglesCounter, decrementTogglesCounter}) => {
                     <div className="flex-1 flex flex-row justify-center items-center">
                         <div className="flex-grow text-center">
                          
-                                <h1 className="text-6xl text-red-900">{data.title.toUpperCase()}</h1>
+                                <h1 style={{fontSize: "300%"}} className="text-red-900">{data.title.toUpperCase()}</h1>
                                 <p className="text-sm mt-4">{data.blurb}</p>
                             
                         </div>
