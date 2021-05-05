687177d6f62fa0c06c0b55cd81f3f0f959406b6e
Tom Stannett <invisibletree@protonmail.ch>
2021-05-06 00:32:54 +0100

    Redundant Code Cleanup

--- src/views/Nimdods.js
+++ src/views/Nimdods.js
@@ -58,51 +58,6 @@ const Nimdods = (props) => {
 
     },[]);
 
-    // animate lines
-    useEffect(() => {
-        const paths = document.getElementsByClassName("line-path-test");
-
-        // vars for upward scrolling behaviour
-        var lastScrollTop = 0;
-
-        // pre specify this -> allows us to address values
-        var lastDrawLengths = new Array(paths.length).fill(0);
-        console.log(lastDrawLengths[0]);
-
-        // scroll event handler
-        const animatePaths = () => {
-            var drawLength = 0;
-            var st = window.pageYOffset || document.documentElement.scrollTop;
-            // check if scrolling down
-            if(st >= lastScrollTop){
-                // handle drawing of each path  
-                for(let i = 0; i < paths.length; i++){
-                    let pathLength = paths[i].getTotalLength();
-                    drawLength = pathLength * ((document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight));
-                    console.log("TEST: " + drawLength);
-                    // only apply if scrolling down, and if beyond current line
-                    /* console.log("draw: " + drawLength + " last: " + lastDrawLengths[i]); */
-                    if(drawLength >= lastDrawLengths[i]){
-                    /*  console.log("path: " + pathLength + " draw: " + drawLength); */
-                        paths[i].style.strokeDashoffset = pathLength - drawLength;
-                    
-                        // update last draw lengths
-                        lastDrawLengths[i] = drawLength;
-                    }
-                }
-            }
-            // update lastScrollTop
-            lastScrollTop = st;
-        }  
-
-        window.addEventListener('scroll', animatePaths, false);
-        
-        return function cleanup() {
-            window.removeEventListener('scroll', animatePaths, false);
-        };
-
-    }, []);
-
     // update DOM
     useEffect(() => {
         if(!loading && CMSTree){
