55b5c13d917b286705ef4a5659551bb32736d83a
Tom Stannett <invisibletree@protonmail.ch>
2021-06-11 22:16:55 +0100

    init commit

--- src/utils/preloadAllAssets.js
+++ src/utils/preloadAllAssets.js
@@ -1,15 +1,27 @@
 import importAll from "./importAll";
 
-const preloadAllAssets = () => {
+const preloadImage = ( name ) => {
+    return new Promise(resolve => {
+      const img = new Image();
+      img.src = name;
+      img.onload = () => {
+        store.dispatch(addSavedimage([name, img])); // <-- dispatch img element itself
+        resolve();
+      };
+    });
+  };
+
+
+const preloadAllAssets = async() => {
     const imgs = importAll(require.context('./../res/', true, /\.(gif|jpe?g|tiff?|png|webp|bmp)$/i));
     const imgTree = []
+    const loadingPromises = []
 
     imgs.forEach((img) => {
-        console.log(img);
-        const newImage = new Image();
-        newImage.src = img.default;
-        imgTree.push(newImage);
+        loadingPromises.push(preloadImage(img));
     })
+
+    await Promise.all(loadingPromises);
 }
 
 export default preloadAllAssets;
