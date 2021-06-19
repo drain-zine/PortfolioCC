98fe9e915edd0dcfdfc59a0205a9501832ff23a7
Tom Stannett <invisibletree@protonmail.ch>
2021-06-19 17:06:46 +0100

    clean up

--- src/utils/preloadAllAssets.js
+++ src/utils/preloadAllAssets.js
@@ -1,22 +1,5 @@
 import importAll from "./importAll";
 
-/* const preloadImage = ( name ) => {
-    return new Promise(resolve => {
-      const img = new Image();
-      img.onload = () => {
-        alert("image is loaded");
-        var index = name.indexOf(this);
-            if (index !== -1) {
-                // remove image from the array once it's loaded
-                // for memory consumption reasons
-                name.splice(index, 1);
-            }
-      };
-
-      img.src = name;
-    });
-  }; */
-
   function preloadImages(array) {
     if (!preloadImages.list) {
         preloadImages.list = [];
@@ -44,15 +27,10 @@ import importAll from "./importAll";
 
 const preloadAllAssets = async() => {
     const imgs = importAll(require.context('./../res/', true, /\.(gif|jpe?g|tiff?|png|webp|bmp)$/i));
-    const imgTree = []
     const loadingPromises = []
     console.log("In preload images")
     preloadImages(imgs);
-   /*  imgs.forEach((img) => {
-        loadingPromises.push(preloadImage(img));
-    }) */
-
-    await Promise.all(loadingPromises);
+    console.log("done");
 }
 
 export default preloadAllAssets;
