c02abbac708fd39c2fe8842090cbe057cdd6e415
Tom Stannett <invisibletree@protonmail.ch>
2021-06-19 13:14:53 +0100

    set Gallery to correct Height

--- src/views/Ephemera.js
+++ src/views/Ephemera.js
@@ -14,6 +14,7 @@ const Ephemera = (props) => {
     const column = ["EPHEMERA", "OBJECT", "EPHEMERA", "TRANSIENCE", "EPHEMERA", "BEING", "EPHEMERA", "FEELING", "EPHEMERA", "SILENCE", "EPHEMERA", "REMINDER", "EPHEMERA"];
   
     const isMount = useIsMount();
+    const gallery = useRef();
 
     const imgN = imgTree.length;
 
@@ -21,6 +22,7 @@ const Ephemera = (props) => {
     const [imgProg, setImgProg] = useState(0);
     const [scrollPercent, setScrollPercent] = useState(0);
     const [imageMain, setImageMain] = useState(0);
+    const [galleryHeight, setGalleryHeight] = useState(0);
 
     
     // increment image counter
@@ -31,8 +33,6 @@ const Ephemera = (props) => {
     const seeAll = (e) => {
         if(!isMount){
             setToggleAll(!toggleAll);
-            //console.log(imageMainCanvasSlideshow);
-            //document.removeEventListener("wheel", getScrollNav, true);
         }
 
     }
@@ -40,11 +40,15 @@ const Ephemera = (props) => {
     const selectImage = (e) => {
         console.log("id:" + e.target.id);
         setImageMain(e.target.id);
+        setToggleAll(!toggleAll);
     }
 
     useEffect(() => {
         setImageMain(Math.round(Math.random() * imgN));
-    }, [])
+        if(gallery){
+            setGalleryHeight(gallery.current.clientHeight);
+        }
+    }, [gallery])
 
     return(
         <main>
@@ -52,22 +56,23 @@ const Ephemera = (props) => {
                 
                 <TextColumn column={column} button={"HOME"}/>
 
-                <div className="galleryContainer w-2/3 border-2 border-white text-white relative overflow-y-scroll">
+                <div ref={gallery} className="galleryContainer w-2/3 border-2 border-white text-white relative overflow-y-scroll">
                     <div id="scroll" className="absolute" style={{left: "10%", top: "50%", transform: "translateY(-50%)"}}>
                         <p className="transform -rotate-90 text-2xl -translate-x-1/2">{scrollPercent}</p>
                     </div>
 
-                    <FadeDiv trigger={!toggleAll} className="z-0 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2"id="expanded">
+                    <FadeDiv trigger={!toggleAll} className="z-0 absolute" id="expanded" style={{height: `${galleryHeight}px`}}>
                         <img src={imgTree[imageMain].src} alt="" className="slide-item__image"/>
                     </FadeDiv>
                     
-                
-                    <FadeDiv trigger={toggleAll} id="gallery" className="z-10">
-                    {imgTree.map((img,i) => (
-                        <FadeDiv trigger={toggleAll} style={{"flex-basis": "33%"}} className=" p-2" onClick={selectImage}>
-                            <img src={(img.src)} className="slide-item__image"  id={i}/>
-                        </FadeDiv>
-                    ))}</FadeDiv>
+                    <div className="scrollWrapper w-full">
+                        <FadeDiv trigger={toggleAll} id="gallery" className="z-10">
+                        {imgTree.map((img,i) => (
+                            <FadeDiv trigger={toggleAll} style={{"flex-basis": "33%"}} className=" p-2" onClick={selectImage}>
+                                <img src={(img.src)} className="slide-item__image"  id={i}/>
+                            </FadeDiv>
+                        ))}</FadeDiv>
+                    </div>
                     
                     
                 </div>
